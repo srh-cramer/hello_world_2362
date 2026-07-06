4afb676f980f39a41f5d18974038b68e8913fc51
srh-cramer <catarina.cramer@srh.de>
2026-07-06 13:07:32 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+void main() {
+    int number; // Deklaration
+    number = 777;   // Initialisierung
+    IO.println(number);
+
+    float floatingNumber = 17.5f;   // Deklaration & Initialisierung
+    IO.println(floatingNumber);
+
+    byte byteNumber = 17;
+    IO.println(byteNumber);
+
+    short cherry = 13000;
+    IO.println(cherry);
+
+    long bigNumber = 10000000000000L;
+    IO.println(bigNumber);
+
+    double doubleNumber = 1.53465754;
+    IO.println(doubleNumber);
+
+    //println = print line
+    IO.println("2362 und 2462 sind eine tolle Gruppe!");
+
+    for (int i = 1; i <= 5; i++) {
+        IO.println("i = " + i);
+    }
+}
